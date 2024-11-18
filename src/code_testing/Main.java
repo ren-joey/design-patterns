a44ca20f900abff5c1c40180298d4a53bceb38c4
ren-joey <tsuyiren@gmail.com>
2024-11-18 13:27:24 +0800

    ci: Set.removeAll testing

---
+++ src/code_testing/Main.java
@@ -0,0 +1,19 @@
+package code_testing;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("A");
+        set.add("B");
+        Set<String> set2 = new HashSet<>();
+        set2.add("B");
+        set2.add("C");
+        set.removeAll(set2);
+        System.out.println(set);
+    }
+}
