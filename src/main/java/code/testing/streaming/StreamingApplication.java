f57cd0d3cf114f2a3259222cedb3e5bab9b2413d
ren-joey <tsuyiren@gmail.com>
2024-12-02 13:51:39 +0800

    feat: 🎸 add streaming test method

---
+++ src/main/java/code/testing/streaming/StreamingApplication.java
@@ -0,0 +1,12 @@
+package code.testing.streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingApplication.class, args);
+    }
+}
---
+++ src/main/java/code/testing/streaming/controller/StreamController.java
@@ -0,0 +1,36 @@
+package code.testing.streaming.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class StreamController {
+
+    @GetMapping("/stream")
+    public StreamingResponseBody stream(HttpServletRequest request) {
+        String times = Optional.ofNullable(request.getParameter("times"))
+                        .filter(s -> !s.isEmpty())
+                        .orElse("1");
+        int intTimes = Integer.parseInt(times);
+
+        return outputStream -> {
+            try {
+                for (int i = 0; i < intTimes; i++) {
+                    outputStream.write((i + " - ").getBytes());
+                    outputStream.flush();
+
+                    TimeUnit.SECONDS.sleep(1);
+                }
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        };
+    }
+}
