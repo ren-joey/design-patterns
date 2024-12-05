d35e866783fa1843344b31f7a0fb3396c938a7f7
ren-joey <tsuyiren@gmail.com>
2024-12-05 10:26:59 +0800

    upt: update the bad pratice for DI testing

--- src/main/java/code/testing/dependency/injection/EmailService.java
+++ src/main/java/code/testing/dependency/injection/EmailService.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+//@RequiredArgsConstructor // good practice for catching NullPointerException
 public class EmailService {
 
-    private final EmailValidator emailValidator;
+    // private final EmailValidator emailValidator; // good practice for catching NullPointerException
+    @Autowired
+    private EmailValidator emailValidator; // bad practice for catching NullPointerException
 
     public void process(String email) {
         if(!emailValidator.isValid(email)){
