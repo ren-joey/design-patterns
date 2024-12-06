b2fb488680cd906a8429e6376cbe7812ed1117db
ren-joey <tsuyiren@gmail.com>
2024-12-06 14:02:39 +0800

    chore: remove some unused file

--- src/main/java/code/testing/dependency/injection/EmailService.java
+++ src/main/java/code/testing/dependency/injection/EmailService.java
@@ -5,15 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-//@RequiredArgsConstructor // good practice for catching NullPointerException
+@RequiredArgsConstructor // good practice for catching NullPointerException
 public class EmailService {
 
-//     private final EmailValidator emailValidator; // good practice for catching NullPointerException
-    @Autowired
-    private EmailValidator emailValidator; // bad practice for catching NullPointerException
+    private final EmailValidator emailValidator; // good practice for catching NullPointerException
+//    @Autowired
+//    private EmailValidator emailValidator; // bad practice for catching NullPointerException
 
     public void process(String email) {
-        if(!emailValidator.isValid(email)){
+        if (!emailValidator.isValid(email)) {
             throw new IllegalArgumentException("INVALID_EMAIL");
         }
     }
